5af7b1c1531249e07fe044a27ebf86dc59f8bbdd
Sebastian Miozga <miozga.sebastian@gmail.com>
2025-02-24 11:10:17 +0100

    abc

---
+++ start/server/server.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.end("<h1>Hello Node!!1234567</h1>");
+});
+
+server.listen(5500, "127.0.0.1", () => console.log("server wystartował"));
